3fb6f64af5936cc49b30bb9401405e83b5ae16c6
Andres Roldan <dresroldan@gmail.com>
2021-03-16 20:11:50 -0500

    imported icons from material-ui and styled header utilizing flexbox for layout

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header__logo" src="https://www.pngitem.com/pimgs/m/12-124076_vinyl-records-png-product-coalition-logo-transparent-png.png"></img>
+      <img
+        className="header__logo"
+        src="https://www.pngitem.com/pimgs/m/12-124076_vinyl-records-png-product-coalition-logo-transparent-png.png"
+      ></img>
+
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        {/* Logo */}
+        <SearchIcon className="header__searchIcon" />
       </div>
+
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
@@ -23,6 +30,10 @@ function Header() {
           <span className="header__optionLineTwo">Collection</span>
         </div>
       </div>
+
+      <div className="header__optionBasket"></div>
+      <AddShoppingCartIcon />
+      <span className="header__optionLineTwo header__basketCount">0</span>
     </div>
   );
 }
