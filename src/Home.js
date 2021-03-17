a646537b185f397718d97b2fbfae7ddd35d90267
Andres Roldan <dresroldan@gmail.com>
2021-03-16 20:47:02 -0500

    created home.js component and add hero image with mask image styling

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images.unsplash.com/photo-1541667558913-5510fb3c7bd9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+          alt="record-shop"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
