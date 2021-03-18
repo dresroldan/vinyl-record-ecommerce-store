0ac410b5c08607b819b931082cd8b03564606c94
Andres Roldan <dresroldan@gmail.com>
2021-03-18 08:54:51 -0500

    installed react-router-dom and created routes for home and checkout pages in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          {/* checkout route */}
+          <Route path="/checkout">
+            <Header />
+            <h1>I am a checkout page</h1>
+          </Route>
+          {/* home page route */}
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
