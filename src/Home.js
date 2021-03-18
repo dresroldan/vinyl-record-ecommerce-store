c8e02742d42d680f8f86f439a84021282a33fa8b
Andres Roldan <dresroldan@gmail.com>
2021-03-18 08:33:19 -0500

    passed props to product.js using the deconstructing method in order to pass the values: title, image, price and rating from product object

--- src/Home.js
+++ src/Home.js
@@ -11,21 +11,76 @@ function Home() {
           src="https://images.unsplash.com/photo-1541667558913-5510fb3c7bd9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
           alt="record-shop"
         />
-      <div className="home__row">
-        <Product />
-        <Product />
-        <Product />
-      </div>
-      <div className="home__row">
-        <Product />
-        <Product />
-        <Product />
-      </div>
-      <div className="home__row">
-        <Product />
-        <Product />
-        <Product />
-      </div>
+        <div className="home__row">
+          <Product
+            id="2563784"
+            title="The Soundwork-Shoppers – Discomusic (LP, RE + CD)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-10043100-1490626390-1896.jpeg.jpg"
+            rating={5}
+          />
+          <Product
+            id="2565784"
+            title="Colin Curtis – Jazz Dance Fusion Volume 2 (LP, Comp)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-16293751-1606728693-5405.jpeg.jpg"
+            rating={5}
+          />
+          <Product
+            id="2523784"
+            title="S-Tone Inc. With Toco & Friends (91) – Onda (LP, Album)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-11457810-1516669109-9012.jpeg.jpg"
+            rating={5}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="2583784"
+            title="Various – Shouts Vol. 2 (12″)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-15049220-1586290876-5199.jpeg.jpg"
+            rating={5}
+          />
+          <Product
+            id="2568784"
+            title="Mar de Luz - All On U (12)"
+            price={19.99}
+            image="https://i1.sndcdn.com/artworks-98c8BLUm7CIz7vxF-DSWXgQ-t500x500.jpg"
+            rating={5}
+          />
+
+          <Product
+            id="3563784"
+            title="Tenorio Jr. – Embalo (LP, Shape, RE)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-10026233-1536320093-4883.jpeg.jpg"
+            rating={5}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="2263784"
+            title="Béliz – Mémoires (LP, Album)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-15693500-1596034993-7924.jpeg.jpg"
+            rating={5}
+          />
+          <Product
+            id="2569784"
+            title="Tommy Guerrero – Soul Food Taqueria (2xLP, Dlx, Ltd, RE, RM, 180)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-11215525-1563382895-2058.jpeg.jpg"
+            rating={5}
+          />
+          <Product
+            id="2563484"
+            title="CW/A – Ringleader EP (12″, EP)"
+            price={19.99}
+            image="https://furtherrecords.com/wp-content/uploads/2021/03/R-9587297-1483358544-3710.jpeg-2.jpg"
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,31 @@
+// import { Title } from "@material-ui/icons";
 import React from "react";
 import "./Product.css";
 
-function Product() {
+// props in es6 allow you to deconstruct the object you pass in.
+// here we are using props to deconstruct title, image, price, and rating from the product object.
+// props allows reusability and efficiency in components.
+
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>Mar de Luz - All On U (12")</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
+
         <div className="product__rating">
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://i1.sndcdn.com/artworks-98c8BLUm7CIz7vxF-DSWXgQ-t500x500.jpg"
-        alt="mardeluz-single"
-      />
+
+      <img src={image} alt="mardeluz-single" />
 
       <button>Add to Basket</button>
     </div>
