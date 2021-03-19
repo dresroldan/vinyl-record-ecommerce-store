3763a65bec443c5b5cf9e5e38c18293f454d2ff5
Andres Roldan <dresroldan@gmail.com>
2021-03-19 08:28:03 -0500

    added styling to CheckoutProduct, img, title price rating and button

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -7,7 +7,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
       <img className="checkoutProduct__image" src={image} />
 
       <div className="checkoutProduct__info">
-        <p className="">{title}</p>
+        <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
           <small>$</small>
           <strong>{price}</strong>
