6e200f28ebb6abf119eacd68b8c8fec48b0423f1
Andres Roldan <dresroldan@gmail.com>
2021-03-18 21:50:03 -0500

    added getBasketTotal function to reducer.js -  includes the reduce array method to iterate through the basket value array, tallys up the price total and returns a value

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
       {/* react component to format number in an input or as a text */}
@@ -10,7 +14,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -18,7 +22,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeperator={true}
         prefix={"$"}
--- src/reducer.js
+++ src/reducer.js
@@ -2,12 +2,15 @@ export const initialState = {
   basket: [],
 };
 
+// reduce method iterates through array and tally up the total and returns a value
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 // reducer dispatches action to data layer
 // global store similiar to Contextprovider
 // reducer listens to actions exa. clicking on add basket and remove from basket
 // spread operator pushes the action.item into the empty basket array
-
-export const reducer = (state, action) => {
+const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
     case "ADD_TO_BASKET":
