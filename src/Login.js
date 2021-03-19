4058d4dede1e91362034bb7325ad005b02c7a2ee
Andres Roldan <dresroldan@gmail.com>
2021-03-19 11:19:30 -0500

    added values and onChange events to both email and password input fields in order to target the users typed info and then capture that value and store iit with useState

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="login">
       <Link to="/">
@@ -17,9 +20,17 @@ function Login() {
 
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="login__signInButton">Sign in</button>
         </form>
         <p>
