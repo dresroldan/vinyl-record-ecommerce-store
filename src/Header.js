b0a3ee513ad0b2bd6e6f2484389f988758db90d5
Andres Roldan <dresroldan@gmail.com>
2021-04-01 11:02:26 -0500

    removed buttons from header.js and added styling to text

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,9 @@ import SearchIcon from "@material-ui/icons/Search";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import TextField from '@material-ui/core/TextField';
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import TextField from "@material-ui/core/TextField";
+import ShoppingCartSharpIcon from "@material-ui/icons/ShoppingCartSharp";
 
 function Header() {
   const [{ basket }, dispatch] = useStateValue();
@@ -18,36 +19,36 @@ function Header() {
           src="https://www.pngitem.com/pimgs/m/260-2609838_vinyl-png-transparent-image-transparent-background-vinyl-record.png"
           alt="record-store"
         />
-
-
       </Link>
 
       <div className="header__search">
-        <input className="header__searchInput" type="text" placeholder="Search artists, albums, and more..."/>
+        <input
+          className="header__searchInput"
+          type="text"
+          placeholder="Search artists, albums, and more..."
+        />
         <SearchIcon className="header__searchIcon" />
-        {/* <TextField  variant="outlined" className="header__searchInput" type="text" label="Search artists, albums, and more..."/> */}
       </div>
+      {/* <TextField  variant="outlined" className="header__searchInput" type="text" label="Search artists, albums, and more..."/> */}
 
       <div className="header__nav">
         <Link to="/login">
           <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+            <span className="header__optionLineTwo">SIGN IN</span>
           </div>
         </Link>
-        <div className="header__option">
+        {/* <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
-        </div>
+        </div> */}
         <div className="header__option">
-          <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineTwo">Collection</span>
+          <span className="header__optionLineTwo">COLLECTION</span>
         </div>
       </div>
 
       <Link to="/checkout">
         <div className="header__optionBasket"></div>
-        <AddShoppingCartIcon />
+        <ShoppingCartSharpIcon />
         <span className="header__optionLineTwo header__basketCount">
           {/* question mark is optional chaining -- if you don't have correct value or basket is undefined it will handle error*/}
           {basket?.length}
--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,14 @@ function Home() {
   return (
     <div className="home">
       <div className="home__container">
+
+<div className="home__image">
+
         <img className="home__image" src={data.heroImage} alt="record-shop" />
+</div>
+
+
+
         <div className="home__row">
           {data.products.map((product) => (
             <Product
