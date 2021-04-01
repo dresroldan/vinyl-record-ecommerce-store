ffbe7b41ec6dc8ae703ef121437401e406fb9855
Andres Roldan <dresroldan@gmail.com>
2021-04-01 13:20:00 -0500

    added material ui appbar, toolbar, badge, iconbutton to header.js

--- src/Header.js
+++ src/Header.js
@@ -1,59 +1,43 @@
 import React from "react";
-import "./Header.css";
-import SearchIcon from "@material-ui/icons/Search";
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
-import AccountBoxIcon from "@material-ui/icons/AccountBox";
-import TextField from "@material-ui/core/TextField";
+import AlbumIcon from "@material-ui/icons/Album";
+import { AppBar, Toolbar, Badge, IconButton } from "@material-ui/core/";
 import ShoppingCartSharpIcon from "@material-ui/icons/ShoppingCartSharp";
+import "./Header.css";
 
 function Header() {
   const [{ basket }, dispatch] = useStateValue();
 
   return (
     <div className="header">
-      <Link to="/">
-        <img
-          className="header__logo"
-          src="https://www.pngitem.com/pimgs/m/260-2609838_vinyl-png-transparent-image-transparent-background-vinyl-record.png"
-          alt="record-store"
-        />
-      </Link>
+      <AppBar color="inherit">
+        <Toolbar>
+          <Link to="/">
+            <AlbumIcon className="header__logo" fontSize="large" />
+          </Link>
 
-      <div className="header__search">
-        <input
-          className="header__searchInput"
-          type="text"
-          placeholder="Search artists, albums, and more..."
-        />
-        <SearchIcon className="header__searchIcon" />
-      </div>
-      {/* <TextField  variant="outlined" className="header__searchInput" type="text" label="Search artists, albums, and more..."/> */}
+          <div className="header__nav">
+            <Link to="/login">
+              <div className="header__option">
+                <span className="header__optionLineOne">SIGN IN</span>
+              </div>
+            </Link>
 
-      <div className="header__nav">
-        <Link to="/login">
-          <div className="header__option">
-            <span className="header__optionLineTwo">SIGN IN</span>
+            <Link to="/checkout">
+              <IconButton aria-label="Show cart items" color="inherit">
+                <Badge
+                  badgeContent={basket?.length}
+                  fontSize="small"
+                  color="secondary"
+                >
+                  <ShoppingCartSharpIcon fontSize="small" />
+                </Badge>
+              </IconButton>
+            </Link>
           </div>
-        </Link>
-        {/* <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">& Orders</span>
-        </div> */}
-        <div className="header__option">
-          <span className="header__optionLineTwo">COLLECTION</span>
-        </div>
-      </div>
-
-      <Link to="/checkout">
-        <div className="header__optionBasket"></div>
-        <ShoppingCartSharpIcon />
-        <span className="header__optionLineTwo header__basketCount">
-          {/* question mark is optional chaining -- if you don't have correct value or basket is undefined it will handle error*/}
-          {basket?.length}
-        </span>
-      </Link>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,19 +1,23 @@
 import React from "react";
+import data from "./data";
+// import Product from "./Product";
 import "./ProductPage.css";
-function ProductPage() {
+
+function ProductPage(props) {
+  // const product = data.products.find((x) => x._id === props.match.params.id);
+ 
+ 
+  // if (!product) {
+  //   return <div>Product not found</div>;
+  // }
+
   return (
     <div className="productPage">
       <div classname="productPage__left">
 
-
-
       </div>
 
-      <div className="productPage__right">
-
-
-          
-      </div>
+      <div className="productPage__right"></div>
     </div>
   );
 }
