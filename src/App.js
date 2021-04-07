c8b80b3831ffe2200bc0ea3191a7693d60315d72
Andres Roldan <dresroldan@gmail.com>
2021-04-06 21:11:50 -0500

    added conditional rendering to header.js wusing context user data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import Signup from "./Signup";
 import { BrowserRouter, Router, Switch, Route } from "react-router-dom";
-// import ProductPage from "./ProductPage";
 import Context from "./Context";
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
           <Route path="/login" component={Login}></Route>
           <Route path="/signup" component={Signup}></Route>
           <Route path="/checkout" component={Checkout}></Route>
-          {/* <Route path="/product" component={ProductPage}></Route> */}
         </Switch>
       </Context>
     </BrowserRouter>
--- src/Header.js
+++ src/Header.js
@@ -1,43 +1,64 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 import AlbumIcon from "@material-ui/icons/Album";
 import { AppBar, Toolbar, Badge, IconButton } from "@material-ui/core/";
 import ShoppingCartSharpIcon from "@material-ui/icons/ShoppingCartSharp";
 import "./Header.css";
+import { myContext } from "./Context";
 
 function Header() {
-  const [{ basket }, ] = useStateValue();
-
-  // const ctx = useContext(myContext);
-
+  const [{ basket }] = useStateValue();
+  const ctx = useContext(myContext);
 
   return (
-    <AppBar position="sticky"  color="inherit">
+    <AppBar position="sticky" color="inherit">
       <Toolbar>
         <Link to="/">
           <AlbumIcon className="header__logo" fontSize="large" />
         </Link>
 
-        <div className="header__nav">
-          <Link to="/login">
-            <div className="header__option">
-              <span className="header__optionLineOne">SIGN IN</span>
-            </div>
-          </Link>
+        {ctx ? (
+          <div className="header__nav">
+            <Link to="/logout">
+              <div className="header__option">
+                <span className="header__optionLineOne">LOGOUT</span>
+              </div>
+            </Link>
 
-          <Link to="/checkout">
-            <IconButton aria-label="Show cart items" color="inherit">
-              <Badge
-                badgeContent={basket?.length}
-                fontSize="small"
-                color="secondary"
-              >
-                <ShoppingCartSharpIcon fontSize="small" />
-              </Badge>
-            </IconButton>
-          </Link>
-        </div>
+            <Link to="/checkout">
+              <IconButton aria-label="Show cart items" color="inherit">
+                <Badge
+                  badgeContent={basket?.length}
+                  fontSize="small"
+                  color="secondary"
+                >
+                  <ShoppingCartSharpIcon fontSize="small" />
+                </Badge>
+              </IconButton>
+            </Link>
+          </div>
+        ) : (
+          <div className="header__nav">
+            <Link to="/login">
+              <div className="header__option">
+                <span className="header__optionLineOne">SIGN IN</span>
+              </div>
+            </Link>
+
+            <Link to="/checkout">
+              <IconButton aria-label="Show cart items" color="inherit">
+                <Badge
+                  badgeContent={basket?.length}
+                  fontSize="small"
+                  color="secondary"
+                >
+                  <ShoppingCartSharpIcon fontSize="small" />
+                </Badge>
+              </IconButton>
+            </Link>
+          </div>
+        )}
       </Toolbar>
     </AppBar>
   );
