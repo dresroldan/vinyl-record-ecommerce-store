dddec76bac01ada6df2fe88c1714053c3b5fe764
Andres Roldan <dresroldan@gmail.com>
2021-04-06 22:53:37 -0500

    ecorrection to conditional rendering in app.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import Signup from "./Signup";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Context from "./Context";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <BrowserRouter>
-      <Context>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/signup" component={Signup}></Route>
-          <Route path="/checkout" component={Checkout}></Route>
-        </Switch>
-      </Context>
-    </BrowserRouter>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+
+        <Route path="/login" component={Login}></Route>
+        <Route path="/signup" component={Signup}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
+      </Switch>
+    </Router>
   );
 }
 
