40f97e2c2f6ce4c3db0a5adebe805f856b3ec457
Andres Roldan <dresroldan@gmail.com>
2021-04-09 15:25:51 -0500

    added type:module to package.json and converted server.js imports

--- backend/data/data.js
+++ backend/data/data.js
@@ -85,4 +85,4 @@ const products = [
   },
 ];
 
-module.exports = products;
+export default products;
--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,58 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const cors = require('cors');
-const passport = require('passport');
-const passportLocal = require('passport-local').Strategy;
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcryptjs');
-const session = require('express-session');
-const User = require('./models/user');
-const products = require('./data/data');
-require('dotenv').config();
+// import mongoose from ('mongoose');
+import express from 'express';
+// import cors from ('cors');
+// import passport from ('passport');
+// import passportLocal from ('passport-local').Strategy;
+// import cookieParser from ('cookie-parser');
+// import bcrypt from ('bcryptjs');
+// import session  from ('express-session');
+// import User from ('./models/user');
+import products from './data/data.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // ------------------------------------- END OF IMPORTS ------------------------------------------- //
 
 // CREATES EXPRESS SERVER
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // MONGO CONNECTION STRING
-const uri = process.env.MONGODB_URI;
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log('Connected to Mongo');
-  })
-  .catch((err) => console.log({ err }));
+// const uri = process.env.MONGODB_URI;
+// mongoose
+//   .connect(uri, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => {
+//     console.log('Connected to Mongo');
+//   })
+//   .catch((err) => console.log({ err }));
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000', // LOCATION OF THE REACT APP WE'RE CONNNECTING TO
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000', // LOCATION OF THE REACT APP WE'RE CONNNECTING TO
+//     credentials: true,
+//   })
+// );
 
 // PARSES JSON
 app.use(express.json());
 // EXPRESS SESSION
-app.use(
-  session({
-    secret: 'secretcode',
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     secret: 'secretcode',
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
-app.use(cookieParser('secretcode'));
-app.use(passport.initialize());
-app.use(passport.session());
-require('./passportConfig')(passport);
+// app.use(cookieParser('secretcode'));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// require('./passportConfig')(passport);
 
 // ------------------------------------- END OF MIDDLEWARE ------------------------------------------- //
 
@@ -66,59 +67,61 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) throw err;
-    if (!user) res.send('No User Exists ');
-    else {
-      req.logIn(user, (err) => {
-        if (err) throw err;
-        res.send('success');
-        console.log(req.user);
-      });
-    }
-  })(req, res, next);
-});
+// app.post('/login', (req, res, next) => {
+//   passport.authenticate('local', (err, user, info) => {
+//     if (err) throw err;
+//     if (!user) res.send('No User Exists ');
+//     else {
+//       req.logIn(user, (err) => {
+//         if (err) throw err;
+//         res.send('success');
+//         console.log(req.user);
+//       });
+//     }
+//   })(req, res, next);
+// });
 
-app.post('/signup', async (req, res) => {
-  const { username, password } = req?.body;
-  if (
-    !username ||
-    !password ||
-    typeof username !== 'string' ||
-    typeof password !== 'string'
-  ) {
-    res.send('Improper Values');
-    return;
-  }
+// app.post('/signup', async (req, res) => {
+//   const { username, password } = req?.body;
+//   if (
+//     !username ||
+//     !password ||
+//     typeof username !== 'string' ||
+//     typeof password !== 'string'
+//   ) {
+//     res.send('Improper Values');
+//     return;
+//   }
 
-  User.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err) throw err;
-    if (doc) res.send('User already Exists');
-    if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//   User.findOne({ username: req.body.username }, async (err, doc) => {
+//     if (err) throw err;
+//     if (doc) res.send('User already Exists');
+//     if (!doc) {
+//       const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-      const newUser = new User({
-        username: req.body.username,
-        password: hashedPassword,
-      });
-      await newUser.save();
-      res.send('User Created');
-    }
-  });
-});
+//       const newUser = new User({
+//         username: req.body.username,
+//         password: hashedPassword,
+//       });
+//       await newUser.save();
+//       res.send('User Created');
+//     }
+//   });
+// });
 
-app.get('/user', (req, res) => {
-  res.send(req.user);
-});
+// app.get('/user', (req, res) => {
+//   res.send(req.user);
+// });
 
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.send('success');
-});
+// app.get('/logout', (req, res) => {
+//   req.logout();
+//   res.send('success');
+// });
 
 // ------------------------------------- END OF ROUTES ------------------------------------------- //
 
-app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on port: ${PORT}`
+  );
 });
