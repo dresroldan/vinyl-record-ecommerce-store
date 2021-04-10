ac88e8946d90c3aeca73f65ef89f667479c2d689
Andres Roldan <dresroldan@gmail.com>
2021-04-10 16:08:11 -0500

    created cart reducer and addToCart action

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      title: data.title,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+    },
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+q;
