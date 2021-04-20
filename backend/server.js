dc08f43aaef8826935dfb225571592ac84c11f62
Andres Roldan <dresroldan@gmail.com>
2021-04-20 13:39:14 -0500

    'deployment troubleshoot

--- backend/server.js
+++ backend/server.js
@@ -111,19 +111,20 @@ app.get('/logout', (req, res) => {
   req.logout();
 });
 
-const __dirname = path.resolve()
+// const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendFile(path.join((__dirname = 'frontend/build/index.html')));
+  });
 }
+
+// build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(
